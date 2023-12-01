fa636ae32094bd8951666c24aaab67c4a013e0c7
anita254 <thisisanne13@gmail.com>
2023-12-01 17:38:13 +0300

    changing allowed hosts

--- my_site/settings.py
+++ my_site/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'l1s5h%-io2b)u&4__&ms1g=x@b0v+!a!o2z6#sl!t$l45$ah^6'
 DEBUG = getenv("IS_DEVELOPMENT", True)
 
 ALLOWED_HOSTS = [
-    getenv("APP_HOST", "localhost")
+    getenv("127.0.0.1",".vercel.app")
 ]
 
 
